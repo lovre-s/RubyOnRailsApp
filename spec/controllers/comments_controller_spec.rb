2c45041ffb1455e28f6877f431a1df46ddf99b40
Lovre <lovres112@gmail.com>
2022-05-23 20:55:17 +0200

    Factory Bot added

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe CommentsController, type: :controller do
 
     describe 'updating' do
         it 'successfully updates comment on an article' do
-          user = User.create(id: 1, username: "root", email: "example@mail.com", password: "123456", password_confirmation: "123456")
-          article = Article.create(id: 1, title: "naslov", body: "tekst", user_id: 1) 
-          @comment = Comment.create(id: 1, body: "komentar", user_id: 1, article_id: 1)
-          @comment.update(:body => "novi komentar")
-          expect(Comment.find_by_body("novi komentar")).to eq(@comment)
+          #user = User.create(id: 1, username: "root", email: "example@mail.com", password: "123456", password_confirmation: "123456")
+          #article = Article.create(id: 1, title: "naslov", body: "tekst", user_id: 1) 
+          #comment = Comment.create(id: 1, body: "komentar", user_id: 1, article_id: 1)
+          comment.update(:body => "novi komentar")
+          expect(Comment.find_by_body("novi komentar")).to eq(comment)
         end
     end
 
